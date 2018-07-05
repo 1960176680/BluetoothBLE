a4309eb5568bc7ee466f9b4e2bceb4bcedaa5d67
Sun <1960176680@qq.com>
2018-07-05 18:00:34 +0800

    蓝牙低功耗初始化提交

--- app/src/main/java/com/zhouwenguang/hz/bluetoothble/MainActivity.java
+++ app/src/main/java/com/zhouwenguang/hz/bluetoothble/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zhouwenguang.hz.bluetoothble;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -7,21 +8,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final long SCAN_PERIOD = 10000;
     private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler = new Handler();
     String uuid1 = "a90924c1-47bd-43ae-a5f4-c3a732830f33";
     String uuid2 = "024105f1-11e0-4482-9306-ad8923e8b4e8";
     String uuid3 = "2c3d5aba-6aa5-4f11-b19d-37c584e00d0c";
     private ArrayAdapter arrayAdapter;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
         arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, "不支持低功耗蓝牙", Toast.LENGTH_SHORT).show();
             finish();
@@ -32,23 +40,37 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, 0x0001);
         }
-        if (mBluetoothAdapter.isEnabled()) {
-//            UUID UUID1=UUID.fromString(uuid1);
-//            UUID UUID2=UUID.fromString(uuid2);
-//            UUID UUID3=UUID.fromString(uuid3);
-//            UUID[] uuids=new UUID[3];
-            mBluetoothAdapter.startLeScan(leScanCallback);
-
-        }
-
-
-
     }
 
     BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
             arrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            arrayAdapter.notifyDataSetChanged();
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0x0001 && resultCode == Activity.RESULT_OK) {
+            scanLeDevice(true);
+
+        }
+    }
+
+    private void scanLeDevice(boolean enable) {
+        if (enable) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mBluetoothAdapter.stopLeScan(leScanCallback);
+                }
+            }, SCAN_PERIOD);
+            mBluetoothAdapter.startLeScan(leScanCallback);
+        } else {
+            mBluetoothAdapter.stopLeScan(leScanCallback);
+        }
+
+
+    }
 }
